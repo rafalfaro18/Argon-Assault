95c238d8c177637b087a81ecec0c9171e5041fb4
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-23 00:12:40 -0600

    Migrates input to New Input System

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void OnEnable() {
+        movement.Enable();
+    }
+
+    void OnDisable() {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+        //float horizontalThrow = Input.GetAxis("Horizontal");
+        //float verticalThrow = Input.GetAxis("Vertical");
         Debug.Log(horizontalThrow);
         Debug.Log(verticalThrow);
     }
