9f7f8c6407221eff334eeabc0dfc32a79121285e
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-23 16:26:23 -0600

    move player with input axis

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     
+    [SerializeField] float controlSpeed = 10f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,11 @@ public class PlayerControls : MonoBehaviour
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
 
-        float xOffset = .1f;
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
         float newXPos = transform.localPosition.x + xOffset;
+        float newYPos = transform.localPosition.y + yOffset;
 
-        transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);    
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);    
     }
 }
