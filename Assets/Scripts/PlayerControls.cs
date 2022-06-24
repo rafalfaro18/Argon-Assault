6291e6a9c0a381c2511d96e788442e46e3ccd947
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-24 14:47:47 -0600

    Adds variables tooltips and headers in editor

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,13 +6,24 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     
+    [Header("General Setup Settings")]
+    [Tooltip("How fast ship moves up and down based upon player input")]
     [SerializeField] float controlSpeed = 30f;
+    [Tooltip("How far player moves horizontally")]
     [SerializeField] float xRange = 10f;
+    [Tooltip("How far player moves vertically")]
     [SerializeField] float yRange = 7f;
+
+    [Header("Laser gun array")]
+    [Tooltip("Add all player lasers here")]
     [SerializeField] GameObject[] lasers;
+    
+    [Header("Screen position based tuning")]
     [SerializeField] float positionPitchFactor = -2f;
-    [SerializeField] float controlPitchFactor = -15f;
     [SerializeField] float positionYawFactor = 2f;
+
+    [Header("Player input based tuning")]
+    [SerializeField] float controlPitchFactor = -15f;
     [SerializeField] float controlRollFactor = -20f;
 
     float xThrow, yThrow;
