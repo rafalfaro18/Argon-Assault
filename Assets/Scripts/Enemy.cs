ce57c41244d9e27d8586be020b293ffe5a50ac6e
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-24 20:34:07 -0600

    Track score

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,16 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent; // Because an empty object only has this.
+    [SerializeField] int scorePerHit = 15;
+    
+    ScoreBoard scoreBoard; // Not serialized because there's only one.
 
+    void Start() {
+        scoreBoard = FindObjectOfType<ScoreBoard>(); // Only one score board, the first result.
+    }
+    
     void OnParticleCollision(GameObject other) {
+        scoreBoard.IncreaseScore(scorePerHit);
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+    
+    public void IncreaseScore(int amountToIncrease) {
+        score += amountToIncrease;
+        Debug.Log($"Score is now: {score}");
+    }
+
+}
