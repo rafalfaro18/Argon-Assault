b3f34ce76752f5c3b913866567a75e70e3db5380
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-24 22:13:12 -0600

    Refactor

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,38 +7,33 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent; // Because an empty object only has this.
     [SerializeField] int scorePerHit = 15;
-    [SerializeField] int hitPoints = 30;
+    [SerializeField] int hitPoints = 10;
     
     ScoreBoard scoreBoard; // Not serialized because there's only one.
 
     void Start() {
         scoreBoard = FindObjectOfType<ScoreBoard>(); // Only one score board, the first result.
     }
-
-    void Update () {
-        GetComponent<MeshRenderer>().material.color = Color.white;
-    }
     
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if (hitPoints < 1) {
+            KillEnemy();
+        }
     }
 
     private void ProcessHit()
     {
-        scoreBoard.IncreaseScore(scorePerHit);
-        GetComponent<MeshRenderer>().material.color = Color.magenta;
         hitPoints--;
+        scoreBoard.IncreaseScore(scorePerHit);
     }
 
     private void KillEnemy()
     {
-        if (hitPoints == 0) {
             GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
             vfx.transform.parent = parent;
             Destroy(gameObject);
-        }
     }
 
 }
